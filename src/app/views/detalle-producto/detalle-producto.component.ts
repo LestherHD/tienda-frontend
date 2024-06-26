b85c227ebb55fdddb834afb7a0021fe5e3b54e0d
LestherHD <wmentel@miumg.edu.gt>
2024-06-25 23:49:40 -0600

    Finalización de detalle de producto

--- src/app/views/detalle-producto/detalle-producto.component.ts
+++ src/app/views/detalle-producto/detalle-producto.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ButtonDirective, CardBodyComponent, CardComponent, FormControlDirective} from '@coreui/angular';
+import {AlertComponent, ButtonDirective, CardBodyComponent, CardComponent, FormControlDirective} from '@coreui/angular';
 import {CustomSpinnerComponent} from '../utils/custom-spinner/custom-spinner.component';
 import {NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
 import {CommonModule} from '@angular/common';
@@ -11,22 +11,27 @@ import {FunctionsUtils} from '../../utils/FunctionsUtils';
 import {Productos} from '../../bo/Productos';
 import {ActivatedRoute} from '@angular/router';
 import {ProductosRequestDTO} from '../../dto/ProductosRequestDTO';
+import {Caracteristicas} from '../../bo/Caracteristicas';
 
 @Component({
   selector: 'app-detalle-producto',
   standalone: true,
   imports: [CardComponent, CardBodyComponent, ButtonDirective, CustomSpinnerComponent, NgbPaginationModule,
-    CommonModule, FormControlDirective, ReactiveFormsModule, ButtonDirective, ButtonsComponent, IconComponent, IconDirective],
+    CommonModule, FormControlDirective, ReactiveFormsModule, ButtonDirective, ButtonsComponent, IconComponent, IconDirective,
+    AlertComponent],
   templateUrl: './detalle-producto.component.html',
   styleUrl: './detalle-producto.component.scss'
 })
 export class DetalleProductoComponent implements OnInit{
 
   producto: Productos;
-  private idProduct: any;
+  idProduct: any;
+  listaCaracteristicas: Caracteristicas[];
+  agregado: boolean;
 
   constructor(private service: Services, functionUtils: FunctionsUtils,
               private route: ActivatedRoute) {
+    this.agregado = false;
   }
 
   ngOnInit(): void {
@@ -38,6 +43,12 @@ export class DetalleProductoComponent implements OnInit{
   }
 
   cargarDatos(){
+
+    this.service.getAllItemsFromEntity('caracteristicas').subscribe( (res: Caracteristicas[])=>{
+      this.listaCaracteristicas = res;
+    }, error => {
+      console.error(error);
+    });
 
     const requestDTO: ProductosRequestDTO = new ProductosRequestDTO(new Productos(this.idProduct, 'Prueba', null, null, null, null, null,
       null, null), null, null);
@@ -52,4 +63,23 @@ export class DetalleProductoComponent implements OnInit{
 
   }
 
+  getValoresPorIdCaracteristica(idCaracteristica: number): string[] {
+    return this.producto.caracteristicas
+      .filter(item => item.caracteristica.id === idCaracteristica)
+      .map(item => item.valor);
+  }
+
+  filtrarCaracteristicasAsociadas(): Caracteristicas[] {
+    // Obtener los IDs de las características asociadas
+    const idsAsociados = this.producto.caracteristicas.map(pc => pc.caracteristica.id);
+
+    // Filtrar la lista de características para incluir solo las que tienen IDs asociados
+    return this.listaCaracteristicas.filter(c => idsAsociados.includes(c.id));
+  }
+
+
+  agregarACarrito() {
+    this.agregado = true;
+    // localStorage.setItem('usuario', JSON.stringify(usuario));
+  }
 }
